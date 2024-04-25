83b534b3bc1d57dbba01102173911f1e8b86b316
13rian Vargas <brianstevenvargasclavijo@gmail.com>
2024-04-25 10:31:48 -0500

    Inicialización de código en Libro, Biblioteca y Principal.

--- src/main/java/com/mycompany/bibliotecapoo/Biblioteca.java
+++ src/main/java/com/mycompany/bibliotecapoo/Biblioteca.java
@@ -1,5 +1,33 @@
 package com.mycompany.bibliotecapoo;
 
+import java.util.LinkedList;
+
 public class Biblioteca {
-    // TODO: Aquí va tu código
+    private LinkedList<Libro> libros;
+
+    public Biblioteca(){
+        this.libros = new LinkedList<>();
+    }//cierra Biblioteca
+    
+    public void registrarLibro(Libro nuevoLibro){
+        libros.add(nuevoLibro);
+    }//cierra registrarLibro
+
+    public void buscarLibro(String palabraBusqueda){
+        for (Libro libro : libros) {
+            if(libros.contains(palabraBusqueda)){
+                mostrarLibros();
+            }else{
+                System.out.println("No existe el libro en la Biblioteca.");
+            }//cierra if
+        }//cierra for
+    }//cierra buscarLibros
+
+    public void mostrarLibros(){
+        
+    }//cierra mostrarLibros
+
+    public void mostrarLibrosNoLeidos(){
+
+    }//cierra mostrarLibrosNoLeidos
 }
--- src/main/java/com/mycompany/bibliotecapoo/Libro.java
+++ src/main/java/com/mycompany/bibliotecapoo/Libro.java
@@ -1,5 +1,32 @@
 package com.mycompany.bibliotecapoo;
 
+import java.time.LocalDate;
+
 public class Libro {
-    // TODO: Aquí va tu código
-}
+    private String titulo;
+    private String autor;
+    private int anoPublicacion;
+    private String genero;
+    boolean leido;
+
+    Libro (String titulo, String autor, int anoPublicacion, String genero){
+        this.titulo = titulo;
+        this.autor = autor;
+        this.anoPublicacion = anoPublicacion;
+        this.genero = genero;
+        this.leido = false;
+    }//cierra Libro
+
+    public String mostrarInformacion(){//Complejidad Temporal: Tiempo constante O(1)
+        return "Título: " + titulo + ", Autor: " + autor + ", Año de publicación: " + anoPublicacion + ", Genero: " + genero;
+    }//cierra mostrarInformacion
+
+    public void marcarLeido(){//Complejidad Temporal: Tiempo constante O(1)
+        leido = true;
+    }//cierra marcarLeido
+
+    public boolean esAntiguo(){//Complejidad Temporal: Tiempo constante O(1)
+        return (LocalDate.now().getYear() - anoPublicacion > 50);
+    }//cierra esAntiguo
+    
+}//cierra class
